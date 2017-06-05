8a59d869c909c86f22ce21521ca72f0946d09d10
Colin Moore <col@orinocoelm.com>
2017-06-05 15:20:00 +0100

    adding smooth scroll from jquery

--- app/assets/scripts/modules/App.js
+++ app/assets/scripts/modules/App.js
@@ -1,7 +1,9 @@
 import MobileMenu from 'MobileMenu';
 import RevealOnScroll from 'RevealOnScroll';
+import StickyHeader from 'StickyHeader';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
