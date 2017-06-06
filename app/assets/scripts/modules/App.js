8b59feafb3c824a96ee50296eb613e0aa06952f6
Colin Moore <col@orinocoelm.com>
2017-06-06 20:59:22 +0100

    Completed Modal overlay.

--- app/assets/scripts/modules/App.js
+++ app/assets/scripts/modules/App.js
@@ -1,9 +1,11 @@
 import MobileMenu from 'MobileMenu';
 import RevealOnScroll from 'RevealOnScroll';
 import StickyHeader from 'StickyHeader';
+import Modal from 'Modal';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
